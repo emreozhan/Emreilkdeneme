a87740375c049c838cacc59c0fb75e901dc19f11
emreozhan <emreozhan71@gmail.com>
2017-02-17 02:43:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/emre111111/emreilkdeneme/ilkactive.java
@@ -0,0 +1,13 @@
+package com.example.emre111111.emreilkdeneme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ilkactive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ilkactive);
+    }
+}
